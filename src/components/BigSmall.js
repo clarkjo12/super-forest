5550bf6510fb08259f34ddd3c76629c91b35bc6b
Jon <clarkjo12@gmail.com>
2020-07-25 19:13:17 -0400

    portfolio polished & React-Spring implemented

--- src/components/BigSmall.js
+++ src/components/BigSmall.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 export default class BigSmall extends React.PureComponent {
   render() {
--- src/components/CallNumber.js
+++ src/components/CallNumber.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 export default class CallNumber extends React.PureComponent {
   render() {
